9c8f606b3c88d6d2dd6b8a752bf454e2f8f51788
twesigyemartha <twesimartha@gmail.com>
2019-04-06 10:47:14 +0300

    added the back end logic code

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,26 @@
     Author     : TWESIGYE MARTHA
      
 */
+
+//back-end
+
+var player1;
+var player2;
+
+var diceRoller = function () {
+    return Math.floor((Math.random() * 6) + 1);
+    // return Math.floor(6*Math.random())+1;
+}
+function Player(changer) {
+    this.roll = 0;
+    this.currentpoints = 0;
+    this.totalpoints = 0;
+    this.changer = changer;
+}
+// game Start
+Player.prototype.gameStart = function () {
+    this.roll = 0;
+    this.currentpoints = 0;
+    this.totalpoints = 0;
+    $("#player1-section, #player2-section").removeClass("winner");
+}
