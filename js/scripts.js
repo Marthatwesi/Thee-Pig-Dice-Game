074c3d3df47eb1a84d0258142a6177b30a0426e8
twesigyemartha <twesimartha@gmail.com>
2019-04-06 10:49:35 +0300

    added demands for holding the die and checking for the winner

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,27 @@ Player.prototype.gameStart = function () {
     this.totalpoints = 0;
     $("#player1-section, #player2-section").removeClass("winner");
 }
+// tossing the die checking for 1 and switching turns
+Player.prototype.dieOne = function () {
+    if (this.roll === 1) {
+        this.currentpoints = 0;
+        alert("sorry you rolled a one, switch to the other player");
+    } else {
+        this.currentpoints += this.roll;
+
+    }
+}
+// holding the die
+Player.prototype.holdDie = function () {
+    this.totalpoints += this.currentpoints;
+    this.currentpoints = 0;
+    alert("your turn is over, switch to the other player");
+}
+
+// checking for the winner
+Player.prototype.checkWinner = function () {
+    while (this.totalpoints >= 100) {
+        alert(" you won the game buddy!");
+        $("#player1-section, #player2-section").addClass("winner");
+        gameStart();
+    }
